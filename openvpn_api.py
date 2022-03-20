910b61845ac013ad54c64a01400257b5f86cdb52
Sergei Samokhvalov <kolobok205@gmail.com>
2022-03-21 00:38:14 +0300

    openvpn api basic integration

--- openvpn_api.py
+++ openvpn_api.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
 
+import requests
+
 
 @dataclass
 class BuildClient:
@@ -12,16 +14,46 @@ class ClientEntity:
     ovpn_entity: str
 
 
+class CreateClientError(ValueError):
+
+    def __init__(self, name: str):
+        self.name = name
+
+
+class GetClientError(ValueError):
+
+    def __init__(self, name: str):
+        self.name = name
+
+
 class OpenvpnApi:
 
-    def __init__(self, uri: str, secure_ca_password: str):
-        self.__ur = uri
-        self.__secure_ca_password = secure_ca_password
+    def __init__(self, uri: str):
+        self.__uri = uri
 
     def create_client(self, build_client: BuildClient) -> ClientEntity:
-        # TODO implement http call
-        return ClientEntity('')
+        try:
+            response = requests.post(f"{self.__uri}/ovpn-config",
+                                     params={'clientId': build_client.name, 'password': build_client.password})
+            if response.status_code == 200:
+                return ClientEntity(response.text)
+            else:
+                raise CreateClientError(build_client.name)
+        except Exception as e:
+            # todo exception logging
+            raise CreateClientError(build_client.name)
 
     def get_client(self, name: str) -> ClientEntity:
-        # TODO implement http call
-        return ClientEntity('')
+        response = requests.get(f"{self.__uri}/ovpn-config",
+                                params={'clientId': name})
+        if response.status_code == 200:
+            return ClientEntity(response.text)
+        else:
+            raise GetClientError(name)
+
+
+if __name__ == '__main__':
+    openvpn_api = OpenvpnApi('http://localhost:8090')
+    e = openvpn_api.create_client(BuildClient('test102', 'kek'))
+    # e = openvpn_api.get_client('new_client1')
+    print(e)
