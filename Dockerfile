ec5527e36a9112d42b1317cb6e78d3e9063c6e33
Igor Shevchenko <baplok@yandex.ru>
2022-03-27 18:45:04 +0300

    rename main to app, add wsgi module

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,4 @@ COPY . /app
 WORKDIR /app
 EXPOSE 8080/tcp
 RUN pip install -r requirements.txt
-CMD ["python3", "main.py"]
+CMD ["make up"]
--- app.py
+++ app.py
@@ -17,14 +17,12 @@ logging.basicConfig(level=config.get('logging', 'level', fallback='INFO'),
 persist = Persistent(config['sqlite']['FilePath'])
 openvpn_api = OpenvpnApi(config['openvpn-http-api']['Uri'])
 admin_manager = AdminManager(openvpn_api, persist)
-app = Flask(__name__)
-
 admin_controller = AdminController(admin_manager, config['admin']['XApiKey'])
 invite_controller = InviteCodeController(admin_manager)
 
-app.register_blueprint(admin_controller.blueprint())
-app.register_blueprint(invite_controller.blueprint())
-
-if __name__ == '__main__':
-    app.run(port=8080, host='0.0.0.0')
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(admin_controller.blueprint())
+    app.register_blueprint(invite_controller.blueprint())
+    return app
 
