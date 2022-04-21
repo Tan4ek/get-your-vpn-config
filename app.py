89a7b9822280afd0c3fceec0db4a5e49e489ba79
Tan4ek <kolobok205@gmail.com>
2022-04-21 09:44:55 +0400

    basic scheduler

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import configparser
 import logging
+import threading
+import time
 
+import schedule
 from flask import Flask
 
 from controller.admin_controller import AdminController
@@ -24,8 +27,39 @@ admin_controller = AdminController(invite_service, config['admin']['XApiKey'])
 invite_controller = InviteController(invite_service, config['get-your-vpn-config']['Host'])
 
 
+class ScheduleThread(threading.Thread):
+
+    def __init__(self, interval: float = 0.1):
+        super().__init__()
+        self._event: threading.Event = threading.Event()
+        self._interval = interval
+
+    def run(self):
+        while not self.stopped():
+            schedule.run_pending()
+            time.sleep(self._interval)
+
+    def stop(self):
+        print("stop is called")
+        self._event.set()
+
+    def stopped(self):
+        return self._event.is_set()
+
+
+def background_job():
+    print('Hello from the background thread')
+
+
+schedule.every(2).seconds.do(background_job)
+
+
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(admin_controller.blueprint())
     app.register_blueprint(invite_controller.blueprint())
+
+    continuous_thread = ScheduleThread()
+    continuous_thread.daemon = True
+    continuous_thread.start()
     return app
